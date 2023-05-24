32d4e81fa1699ef0927812abfb6e47f15ca33727
smdoo0 <110598989+smdoo0@users.noreply.github.com>
2023-05-24 22:55:41 +0900

    저장4
    .

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, redirect, url_for, render_template,request, abort, flas
 from pymongo import MongoClient
 cluster = MongoClient("mongodb+srv://smdoo:Me2sChTXYh49P3Lk@cluster0.ydrdzo1.mongodb.net/?retryWrites=true&w=majority")
 db = cluster["software_engineering"]
-collection = db["test"]
+users = db["test"]
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "누구도알수없는보안이진짜최고인암호키"
@@ -23,21 +23,28 @@ def mypage():
     return render_template('mypage.html')
 
 #로그인 후 메인 페이지
-@app.route('/main_after_login')
+#@app.route('/main_after_login')
+#def main_after_login():
+#    if request.method == 'POST':
+#        if 
+
+@app.route('/afterlogin')
 def main_after_login():
+    
     return render_template('main_after_login.html')
 
-#로그인
+# #로그인
 @app.route('/login_new', methods = ['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        if collection.find_one({"_id":request.form['id']}):
+        if users.find_one({"_id":request.form['id']}):
             id = request.form['id']
-            id_list = collection.find_one({"_id":id})
+            id_list = users.find_one({"_id":id})
             if request.form['pw'] == id_list['pw']:
                 session['id'] = id
                 flash('You have logged in successfully as {}'.format(id))
-                return render_template('main_after_login.html') 
+                name=id_list['name']
+                return render_template('main_after_login.html', name=name) 
             else:
                 flash("올바르지 않은 비밀번호입니다!")
                 return render_template('login_new.html')
@@ -46,6 +53,23 @@ def login():
             return render_template('signup.html')
     else:
         return render_template('login_new.html')  
+#로그인(재훈)
+# @app.route('/login_new', methods = ['POST', 'GET'])
+# def login():
+#     if request.method == 'POST':
+#         id = request.form['id']
+#         pw = request.form['pw']
+    
+#     user = users.find_one({'_id':id,'pw':pw})
+    
+#     if user is None:
+#         flash("아이디와 비밀번호를 다시 한 번 확인해주세요.")
+#         return render_template('login_new.html')
+    
+#     flash("로그인 성공!")  
+#     return render_template('main_after_login.html',username=username)
+              
+        
 
 # 회원가입
 @app.route('/signup', methods=['POST', 'GET'])
@@ -63,13 +87,13 @@ def signup():
             return render_template('signup.html')
         
         # 이미 존재하는 id인지 확인
-        existing_user = collection.find_one({"_id": id})
+        existing_user = users.find_one({"_id": id})
         if existing_user:
             flash("이미 존재하는 id입니다.")
             return render_template('signup.html')
 
         # MongoDB에 데이터 삽입
-        collection.insert_one({"_id": id, "pw": password, "name": username})
+        users.insert_one({"_id": id, "pw": password, "name": username})
 
         return redirect('/')  # 회원가입 후 메인 페이지로 리디렉션
 
